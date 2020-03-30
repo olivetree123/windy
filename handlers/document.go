e9d85a5016724d155cbf1a113a85d7614312a4a1
olivetree123 <olivetree123@163.com>
2020-03-30 18:31:39 +0800

    fix bug

--- handlers/document.go
+++ handlers/document.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
+	"net/http"
 	"windy/index"
 	"windy/log"
 	"windy/models"
@@ -8,11 +11,40 @@ import (
 	"github.com/olivetree123/coco"
 )
 
+// DocCreateParam 创建文档参数
+type DocCreateParam struct {
+	DbID    string
+	Content string
+}
+
+// Validate 参数验证
+func (param *DocCreateParam) Validate() (bool, error) {
+	if param.DbID == "" {
+		return false, errors.New("db_id should not be null")
+	}
+	if param.Content == "" {
+		return false, errors.New("content should not be null")
+	}
+	return true, nil
+}
+
+// Load 加载参数
+func (param *DocCreateParam) Load(request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	err := decoder.Decode(param)
+	if err != nil {
+		log.Error(err)
+		return coco.ErrorResponse(100)
+	}
+}
+
 // DocCreateHandler 创建文档
 func DocCreateHandler(c *coco.Coco) coco.Result {
-	dbID := c.Params.ByName("db_id")
-	content := c.Params.ByName("content")
-	if dbID == "" || content == "" {
+	var param DocCreateParam
+	param.Load(c.Request)
+	status, err := param.Validate()
+	if !status {
+		log.Error(err)
 		return coco.ErrorResponse(100)
 	}
 	doc, err := models.NewDocument(dbID, content)
